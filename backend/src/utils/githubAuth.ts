ab1f9718118f55a56a2ce857712c68c2c10149dd
Tushar Sharma <sharmatushar811@gmail.com>
2025-09-25 17:12:28 +0530

    ENV fix

--- backend/src/utils/githubAuth.ts
+++ backend/src/utils/githubAuth.ts
@@ -3,10 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 export function generateAppJwt(): string {
   const now = Math.floor(Date.now() / 1000);
-
+  const privateKey = process.env.GITHUB_APP_PRIVATE_KEY?.replace(/\\n/g, "\n");
+  console.log("Private Key:", privateKey); // Debugging line
+  
+  if (!privateKey) {
+    throw new Error("GITHUB_APP_PRIVATE_KEY is not defined");
+  }
   return jwt.sign(
     { iat: now - 60, exp: now + 600, iss: process.env.GITHUB_APP_ID },
-    process.env.GITHUB_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+    privateKey,
     { algorithm: "RS256" }
   );
 }
