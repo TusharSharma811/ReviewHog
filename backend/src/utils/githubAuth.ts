aeaf1f743c23bdb81175b06f7163338fef590edd
Tushar Sharma <sharmatushar811@gmail.com>
2025-09-25 17:23:40 +0530

    Trying.....

--- backend/src/utils/githubAuth.ts
+++ backend/src/utils/githubAuth.ts
@@ -1,14 +1,18 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import crypto from "crypto";
 dotenv.config();
+
 export function generateAppJwt(): string {
-  const now = Math.floor(Date.now() / 1000);
-  const privateKey = process.env.GITHUB_APP_PRIVATE_KEY?.replace(/\\n/g, "\n");
-  console.log("Private Key:", privateKey); // Debugging line
-  
-  if (!privateKey) {
-    throw new Error("GITHUB_APP_PRIVATE_KEY is not defined");
+  if (!process.env.GITHUB_APP_PRIVATE_KEY) {
+    throw new Error("GITHUB_APP_PRIVATE_KEY is not set");
   }
+
+  const now = Math.floor(Date.now() / 1000);
+  const keyString = process.env.GITHUB_APP_PRIVATE_KEY.replace(/\\n/g, "\n");
+
+  const privateKey = crypto.createPrivateKey({ key: keyString, format: "pem" });
+
   return jwt.sign(
     { iat: now - 60, exp: now + 600, iss: process.env.GITHUB_APP_ID },
     privateKey,
