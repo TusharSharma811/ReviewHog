322e10805cf899bd80fe5acbf7626eac8a95f91e
Tushar Sharma <sharmatushar811@gmail.com>
2025-11-02 13:55:23 +0530

    .

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import prisma from "../db/prismaClient.js";
-
+import { Repo } from "@prisma/client";
 export const getUserInsights = async (req: Request, res: Response) => {
   const userId = req.query.uid as string;
   if (!userId) {
@@ -12,9 +12,12 @@ export const getUserInsights = async (req: Request, res: Response) => {
       include: {
         repos: { include: { owner: true, reviews: { orderBy: { createdAt: "desc" }, take: 5 } } },
         reviews: {
-                include: { repo: true },
-          orderBy: { createdAt: "desc" },
-          take: 5,
+                include: { repo: true},
+                orderBy: { createdAt: "desc" },
+                take: 5,
+                select:{
+                  repo:{ select: { id: true, name: true, ownerId: true } },
+                }
         },
         insights: true,
       },
@@ -25,6 +28,31 @@ export const getUserInsights = async (req: Request, res: Response) => {
     res.status(200).json(userData);
   } catch (error) {
     console.error("Error fetching user insights:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const toggleGithubReview = async (req: Request, res: Response) => {
+  const userId = req.query.uid as string;
+  const { repoId } = req.params;
+
+  if (!userId || !repoId) {
+    return res.status(400).json({ message: "User ID and Repository ID are required" });
+  }
+
+  try {
+    const repo = await prisma.repo.findUnique({
+      where: { id: repoId },
+    });
+    
+    await prisma.repo.update({
+      where: { id: repoId },
+      data: { isReviewOn: !repo?.isReviewOn },
+    });
+
+    res.status(200).json({ message: "GitHub reviews toggled for repository" });
+  } catch (error) {
+    console.error("Error toggling GitHub reviews:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import verifyJWT from "../middleware/verifyJWT.js";
-import { getUserInsights} from "../controllers/user.controller.js";
+import { getUserInsights , toggleGithubReview} from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.get('/me/insights', verifyJWT, getUserInsights);
+router.post("/github/toggleReview/:repoId", verifyJWT, toggleGithubReview);
 export default router;
